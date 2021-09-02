66ac0e72adf00c15adc09cd88b29f4286c6a5284
Felipe Rossoni <feliperossoni05@gmail.com>
2021-09-02 14:20:35 -0300

    Create exercicio29.py

---
+++ exercicio29.py
@@ -0,0 +1,6 @@
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+n3 = float(input("Digite a segunda nota: "))
+n4 = float(input("Digite a terceira nota: "))
+media = ((n1+n2+n3+n4)/4)
+print("A média aritmética de suas notas é : {}".format(media))
