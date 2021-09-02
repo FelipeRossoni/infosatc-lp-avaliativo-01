29c19da45ae70da946e21e05812a368a3800e939
Felipe Rossoni <feliperossoni05@gmail.com>
2021-09-02 14:23:42 -0300

    Create exercicio30.py

---
+++ exercicio30.py
@@ -0,0 +1,4 @@
+real = float(input("Digite seu dinheiro em reais: "))
+cotacao = float(input("Digite a cotação atual do dólar para reais: "))
+dolar = (real/cotacao)
+print("valor em dólares : {}".format(dolar))
